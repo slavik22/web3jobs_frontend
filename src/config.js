7d3afe6e194c8ddb8aa745b2496e840329cba2df
Yaroslav Fedyna <fedynaslava@gmail.com>
2025-11-04 13:33:39 +0200

    apiBase fix

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const apiBase = 'http://127.0.0.1:5000';
-export const apiBase = 'https://backend-1055266214449.europe-west1.run.app/'
+export const apiBase = 'https://backend-1055266214449.europe-west1.run.app'
